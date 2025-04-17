aaa22eae84cfe38663e2b4f2d0583b8bbdb8f1a1
dhruv5604 <dhruvsolanki5604@gmail.com>
2025-04-17 16:28:29 +0530

    learn about base.html.twig and blocks

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,20 +2,34 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
 
-class VinylController {
+class VinylController extends AbstractController 
+{
     #[Route('/')]
-    public function homepage(): Response {
-        return new Response('<h1>Welcome to homepage</h1>');
+    public function homepage(): Response 
+    {   
+        $tracks = [
+            'Gangstar\'s paradise - Coolio',
+            'Waterfalls - TLC',
+            'Creep - Radiohead',
+            'Kiss from a Ross - Seal',
+            'On Bended Knee - Boyz II men'
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+            "title" => 'PB and Jams',
+            "tracks" => $tracks
+        ]);
     }
 
     #[Route('/browse/{slug}')]
-    public function browse($slug = null): Response {
-
+    public function browse($slug = null): Response 
+    {
         if($slug){
             $title = "Genres: " . u(str_replace('-',' ',$slug))->title(true);
         } else {
