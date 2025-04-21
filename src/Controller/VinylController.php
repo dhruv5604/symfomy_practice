3e458f45ab51372ef4ba6080796578fc6763ecc2
dhruv5604 <dhruvsolanki5604@gmail.com>
2025-04-21 13:58:17 +0530

    added time bundle

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,43 +2,70 @@
 
 namespace App\Controller;
 
+use Knp\Bundle\TimeBundle\DateTimeFormatter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
-
 use function Symfony\Component\String\u;
 
-
-class VinylController extends AbstractController 
+class VinylController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function homepage(Environment $twig): Response 
-    {   
+    public function homepage():  Response   
+    {
         $tracks = [
-            'Gangstar\'s paradise - Coolio',
-            'Waterfalls - TLC',
-            'Creep - Radiohead',
-            'Kiss from a Ross - Seal',
-            'On Bended Knee - Boyz II men'
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
         ];
 
-        $html = $twig->render('vinyl/homepage.html.twig', [
-            "title" => 'PB and Jams',
-            "tracks" => $tracks
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
         ]);
-
-        return new Response($html);
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse($slug = null): Response 
+    public function browse(DateTimeFormatter $dateTimeFormatter, $slug = null): Response
     {
-        $genre = $slug ? u(str_replace('-',' ',$slug))->title(true) : null;
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+        $mixes = $this->getMixes();
+
+        foreach ($mixes as $key => $mix) {
+            $mixes[$key]['ago'] = $dateTimeFormatter->formatDiff($mix['createdAt']); 
+        }
 
         return $this->render('vinyl/browse.html.twig', [
-            'genre'=>$genre,
+            'genre' => $genre,
+            'mixes' => $mixes,
         ]);
     }
 
+    private function getMixes(): array
+    {
+        // temporary fake "mixes" data
+        return [
+            [
+                'title' => 'PB & Jams',
+                'trackCount' => 14,
+                'genre' => 'Rock',
+                'createdAt' => new \DateTime('2021-10-02'),
+            ],
+            [
+                'title' => 'Put a Hex on your Ex',
+                'trackCount' => 8,
+                'genre' => 'Heavy Metal',
+                'createdAt' => new \DateTime('2022-04-28'),
+            ],
+            [
+                'title' => 'Spice Grills - Summer Tunes',
+                'trackCount' => 10,
+                'genre' => 'Pop',
+                'createdAt' => new \DateTime('2019-06-20'),
+            ],
+        ];
+    }
 }
